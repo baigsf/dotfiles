3fc244ac648e91699da5ce3516939e6e0191f96c
Saif バイグ <157822627+baigsf@users.noreply.github.com>
2025-09-17 14:14:07 -0400

    nvim config update

---
+++ nvim/init.lua
@@ -0,0 +1,18 @@
+-- Basic Neovim config
+
+-- Line numbers
+vim.opt.number = true
+
+-- Use your terminal's background (don’t override colorscheme)
+vim.opt.termguicolors = true
+vim.cmd("highlight Normal ctermbg=NONE guibg=NONE")
+vim.cmd("highlight NonText ctermbg=NONE guibg=NONE")
+
+-- Sensible defaults
+vim.opt.relativenumber = false   -- only absolute numbers
+vim.opt.mouse = "a"              -- enable mouse support
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
+
+-- Keymaps for quitting
+vim.keymap.set("n", "ZZ", ":wq<CR>", { silent = true })
+vim.keymap.set("n", "ZQ", ":q!<CR>", { silent = true })
