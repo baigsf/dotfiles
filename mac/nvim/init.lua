52217a3f22c2ee516700c6c4db4234f7aaee98df
Saif バイグ <157822627+baigsf@users.noreply.github.com>
2025-10-21 22:45:57 -0400

    Adding some more stuff

--- mac/nvim/init.lua
+++ mac/nvim/init.lua
@@ -1,28 +1,27 @@
--- Basic Neovim config
+-- init.lua
 
 -- Line numbers
 vim.opt.number = true
 
--- Use your terminal's background (don’t override colorscheme)
+-- Use your terminal's background
 vim.opt.termguicolors = true
 vim.cmd("highlight Normal ctermbg=NONE guibg=NONE")
 vim.cmd("highlight NonText ctermbg=NONE guibg=NONE")
 
 -- Sensible defaults
-vim.opt.relativenumber = false   -- only absolute numbers
-vim.opt.mouse = "a"              -- enable mouse support
-vim.opt.clipboard = "unnamedplus" -- use system clipboard
+vim.opt.relativenumber = false
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
 
 -- Keymaps for quitting
 vim.keymap.set("n", "ZZ", ":wq<CR>", { silent = true })
 vim.keymap.set("n", "ZQ", ":q!<CR>", { silent = true })
 
-
--- apply the colorscheme
-vim.cmd.colorscheme "catppuccin"
-
-require("catppuccin").setup({
-  flavour = "mocha", -- latte, frappe, macchiato, mocha
-  transparent_background = true, -- 👈 this keeps your terminal’s background
+-- Gruvbox setup
+require("gruvbox").setup({
+  -- This is the key setting for a transparent background
+  transparent_mode = "hard",
 })
-vim.cmd.colorscheme "catppuccin"
+
+-- Apply the colorscheme
+vim.cmd.colorscheme "gruvbox"
